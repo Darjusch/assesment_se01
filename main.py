b4940a91b3cc96c60098431882f775680b78aa65
Darjusch <Darjusch.schrand@code.berlin>
2019-12-03 16:41:37 +0100

    updated to 4x4 grid

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 class SlidingPuzzle:
     def __init__(self):
         self.puzzle_horizontal = [
-            [1,8,2],
-            [4,3,5],
-            [7,6,0]
+            [15,2,1,12],
+            [8,5,6,11],
+            [4,9,10,7],
+            [3,14,13,0]
         ]
         '''
         Easy testing for win condition.
@@ -17,14 +18,16 @@ class SlidingPuzzle:
         '''
 
         self.puzzle_vertical = [
-            [1,4,7],
-            [8,3,6],
-            [2,5,0]
+            [15,8,4,3],
+            [2,5,9,14],
+            [1,6,10,13],
+            [12,11,7,0]
         ]
         self.winning_puzzle = [
-            [1,2,3],
-            [4,5,6],
-            [7,8,0]
+            [1,2,3,4],
+            [5,6,7,8],
+            [9,10,11,12],
+            [13,14,15,0]
         ]
 
         self.user_input_and_choices()
@@ -39,7 +42,7 @@ class SlidingPuzzle:
             print('You won the Game !')
             print('Play another Round')
         piece_to_be_moved = int(input(f'Please chose which puzzle piece you want to move by typing the number of it: '))
-        if piece_to_be_moved < 1 or piece_to_be_moved > 9:
+        if piece_to_be_moved < 1 or piece_to_be_moved > 15:
             self.user_input_and_choices()
         if self.is_valid_move(piece_to_be_moved, self.puzzle_horizontal):
             self.swap_places_horizontal(piece_to_be_moved)
@@ -55,7 +58,7 @@ class SlidingPuzzle:
     def is_valid_move(self, piece, puzzle):
         for nested_list in puzzle:
             if piece in nested_list and 0 in nested_list:
-                if nested_list.index(0) == 1 or nested_list.index(piece) == 1:
+                if (nested_list.index(0) - nested_list.index(piece)) == 1 or (nested_list.index(0) - nested_list.index(piece)) == -1:
                     return True
                 else:
                     return False
